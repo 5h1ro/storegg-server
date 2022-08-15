017ff787d977d41d09ccbf96cf013f2fb435962c
5h1ro <nurhakikiajha@gmail.com>
2022-08-15 19:13:51 +0700

    first

---
+++ app/dashboard/controller.js
@@ -0,0 +1,25 @@
+const Transaction = require('../transaction/model')
+const Voucher = require('../voucher/model')
+const Player = require('../player/model')
+const Category = require('../category/model')
+
+module.exports = {
+    index: async (req, res) => {
+        try {
+
+            const transaction = await Transaction.countDocuments();
+            const voucher = await Voucher.countDocuments();
+            const category = await Category.countDocuments();
+            const player = await Player.countDocuments();
+            res.render('admin/dashboard/view_dashboard', {
+                name: req.session.user.name,
+                title: 'Halaman Dashboard',
+                count: {
+                    transaction, voucher, category, player
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
---
+++ app/users/model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+let userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        require: [true, 'Email harus diisi']
+    },
+
+    name: {
+        type: String,
+        require: [true, 'Nama harus diisi']
+    },
+
+    password: {
+        type: String,
+        require: [true, 'Kata sandi harus diisi']
+    },
+
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'admin'
+    },
+
+    phoneNumber: {
+        type: String,
+        require: [true, 'Nomor telepon harus diisi']
+    },
+
+    status: {
+        type: String,
+        enum: ['Y', 'N'],
+        default: 'Y'
+    },
+}, { timestamp: true })
+
+module.exports = mongoose.model('Users', userSchema)

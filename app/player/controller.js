47e99d7092a8221cb215795bcc7ee15c5faf1bd6
5h1ro <nurhakikiajha@gmail.com>
2022-08-17 21:12:57 +0700

    api bank

--- app/player/controller.js
+++ app/player/controller.js
@@ -37,7 +37,7 @@ module.exports = {
                 .populate('nominals')
                 .populate('user', '_id name phoneNumber')
 
-            const payment = await Payment.find()
+            const payment = await Payment.find().populate('banks')
 
             voucher = { detail: voucher, payment }
             if (!voucher) {
